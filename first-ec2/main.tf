306fa0fd9a78564dffcb4168541f9c29e83620b6
Swapnali <88023216+swapnali-datkhile@users.noreply.github.com>
2025-03-24 17:56:37 +0530

    Created my first ec2 instance via terraform
    Created my first ec2 instance via terraform

---
+++ first-ec2/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = ""
+    secret_key = ""
+}
+
+resource "aws_instance" "demo_instance" {
+    ami = "ami-08b5b3a93ed654d19"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "my-ec2"
+    }
+}
