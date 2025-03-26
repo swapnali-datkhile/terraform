76c8993d3392e5dd3d41289a0a3602a357b13a97
Swapnali <88023216+swapnali-datkhile@users.noreply.github.com>
2025-03-26 10:49:21 +0530

    Two more ways of defining output
    Two more ways of defining output

--- outputs-in-terraform/outputs.tf
+++ outputs-in-terraform/outputs.tf
@@ -1,3 +1,13 @@
 output "elastic-public-ip" {
   value = aws_eip.elastic-ip.public_ip
 }
+
+# if you have not decided which specific attribute you want you can use below approach which will give you all values related to resource
+output "elastic-ip-all-details" {
+  value = aws_eip.elastic-ip
+}
+
+# In some cases you want url in output. Use below approach to achieve that requirement.
+output "URL" {
+  value = "https://${aws_eip.elastic-ip.public_ip}:8080"
+}
