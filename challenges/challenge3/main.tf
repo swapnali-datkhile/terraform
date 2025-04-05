998e54b1137f5e7d5be30a8a25fe624d5d6cc105
Swapnali <88023216+swapnali-datkhile@users.noreply.github.com>
2025-04-05 04:50:58 +0000

    Challenge 3 solved

---
+++ challenges/challenge3/main.tf
@@ -0,0 +1,18 @@
+
+variable "instance_config" {
+  type = map(any)
+  default = {
+    instance1 = { instance_type = "t2.micro", ami = "ami-03a6eaae9938c858c" }
+    instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
+  }
+}
+
+resource "aws_instance" "chal-inst" {
+  for_each      = var.instance_config
+  instance_type = each.value.instance_type
+  ami           = each.value.ami
+
+  tags = {
+    Name = each.key
+  }
+}
